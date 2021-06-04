41a124ef84e9929248e7efe78f4a04c29b1c41ee
yunnyisgood <cyeon0801@gmail.com>
2021-06-04 18:03:47 +0900

    today commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import SignUp from './user/SignUp'
+import Login from './user/Login'
+import UserList from './user/UserList'
 
 ReactDOM.render(
   <React.StrictMode>
-    <SignUp />
+    <UserList/>
   </React.StrictMode>,
   document.getElementById('root')
 );
